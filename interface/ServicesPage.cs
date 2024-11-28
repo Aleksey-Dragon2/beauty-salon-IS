5a2d54b245b05e0dfd831944ed8c93f49d098b04
aleksey <aleha2552654@gmail.com>
2024-11-28 23:56:42 +0300

    add first page

---
+++ interface/ServicesPage.cs
@@ -0,0 +1,41 @@
+﻿using MyWindowsFormsApp.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyWindowsFormsApp.Models;
+
+namespace @interface
+{
+    public partial class ServicesPage : Form
+    {
+        private readonly ApiService _apiService;
+        public ServicesPage()
+        {
+            InitializeComponent();
+            var httpClientService = new HttpClientService();
+            _apiService = new ApiService(httpClientService);
+        }
+
+        private void ServicesLabel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Service service = _apiService.GetService(); // Доделать вывод услуг
+
+                NameService.Text = service.Name;
+                PriceService.Text = Convert.ToString(service.Price);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+
+}
