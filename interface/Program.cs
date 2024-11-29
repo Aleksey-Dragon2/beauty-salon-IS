8025438be427bf6c1dbe221b7de5d36148b8e38f
aleksey <aleha2552654@gmail.com>
2024-11-29 22:12:16 +0300

    init

--- interface/Program.cs
+++ interface/Program.cs
@@ -17,6 +17,7 @@ namespace @interface
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ServicesPage());
+
         }
     }
 }
