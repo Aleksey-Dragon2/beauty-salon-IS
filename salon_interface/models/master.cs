0440aa3b7459400c69f8a3b630f0c032ee6ef342
aleksey <aleha2552654@gmail.com>
2024-12-02 23:20:56 +0300

    init

---
+++ salon_interface/models/master.cs
@@ -0,0 +1,10 @@
+﻿namespace MyWindowsFormsApp.Models
+{
+    public class Master
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname {  get; set; }
+        public string Specialization { get; set; }
+    }
+}
